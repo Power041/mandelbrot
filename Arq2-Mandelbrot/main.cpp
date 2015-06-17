8e5c15cac7ec2cd2c410e965a1d1717060b24491
Power041 <Power041@users.noreply.github.com>
2015-06-17 18:28:30 -0300

    Mudança da geração dos pixels
    Pixels agora gerados numa função um a um

--- Arq2-Mandelbrot/main.cpp
+++ Arq2-Mandelbrot/main.cpp
@@ -5,15 +5,16 @@
 
 using namespace std;
 
+const int altura    = 1024;
+const int largura   = 1024;
+
+void mandelbrot(char*, int, int);
 
 int main(int argc, char* argv[])
 {
 	ofstream saida;
 
 	saida.open("mandelbrot.ppm", ios::binary | ios::out);
-
-    const int altura    = 1024;
-    const int largura   = 1024;
 
 	char * header = new char[20];
 
@@ -25,6 +26,30 @@ int main(int argc, char* argv[])
 
     // Mandelbrot
     int iY, iX;
+
+    // buffer for all pixels, removing write overhead each iteration
+    char * color = new char[altura*largura*3];
+
+    for(iY=0;iY<altura;iY++)
+    {
+        for(iX=0;iX<largura;iX++)
+        {
+             mandelbrot(color, iX, iY);
+        }
+    }
+
+    /*write color to the file*/
+    saida.write(color, altura*largura*3);
+	saida.close();
+
+	return 0;
+}
+
+
+void mandelbrot (char * buf, int X, int Y)
+{
+    static int iTamanho = 0;
+
     double Cx, Cy;
     const double CxMin  =   -2.5;
     const double CxMax  =    1.5;
@@ -38,56 +63,39 @@ int main(int argc, char* argv[])
     double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
     /*  */
     int Iteration;
-    const int IterationMax=256;
+    const int IterationMax = 256;
     /* bail-out value , radius of circle ;  */
-    const double EscapeRadius=2;
-    double ER2=EscapeRadius*EscapeRadius;
-    // buffer for all pixels, removing write overhead each iteration
-    char * color = new char[altura*largura*3];
-    int iTamanho = 0;
-
-    for(iY=0;iY<altura;iY++)
+    const double EscapeRadius = 2;
+    double ER2 = EscapeRadius * EscapeRadius;
+    Cy = CyMin + Y * PixelHeight;
+    if (fabs(Cy) < PixelHeight/2) Cy = 0.0; /* Main antenna */
+    Cx = CxMin + X * PixelWidth;
+    /* initial value of orbit = critical point Z= 0 */
+    Zx = 0.0;
+    Zy = 0.0;
+    Zx2 = Zx * Zx;
+    Zy2 = Zy * Zy;
+    /* */
+    for (Iteration = 0; Iteration < IterationMax && ((Zx2 + Zy2) < ER2); Iteration++)
     {
-         Cy=CyMin + iY*PixelHeight;
-         if (fabs(Cy)< PixelHeight/2) Cy=0.0; /* Main antenna */
-         for(iX=0;iX<largura;iX++)
-         {
-                Cx=CxMin + iX*PixelWidth;
-                /* initial value of orbit = critical point Z= 0 */
-                Zx=0.0;
-                Zy=0.0;
-                Zx2=Zx*Zx;
-                Zy2=Zy*Zy;
-                /* */
-                for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++)
-                {
-                    Zy=2*Zx*Zy + Cy;
-                    Zx=Zx2-Zy2 +Cx;
-                    Zx2=Zx*Zx;
-                    Zy2=Zy*Zy;
-                };
-                /* compute  pixel color (24 bit = 3 bytes) */
-                if (Iteration==IterationMax)
-                { /*  interior of Mandelbrot set = black */
-                    color[iTamanho    ]=0;
-                    color[iTamanho + 1]=0;
-                    color[iTamanho + 2]=0;
-                    iTamanho += 3;
-                }
-                else
-                { /* exterior of Mandelbrot set = white */
-                    color[iTamanho    ]=((IterationMax-Iteration) % 8) *  63;  /* Red */
-                    color[iTamanho + 1]=((IterationMax-Iteration) % 4) * 127;  /* Green */
-                    color[iTamanho + 2]=((IterationMax-Iteration) % 2) * 255;  /* Blue */
-                    iTamanho += 3;
-                };
-
-            }
+        Zy = 2 * Zx * Zy + Cy;
+        Zx = Zx2 - Zy2 + Cx;
+        Zx2 = Zx * Zx;
+        Zy2 = Zy * Zy;
+    };
+    /* compute  pixel color (24 bit = 3 bytes) */
+    if (Iteration == IterationMax)
+    { /*  interior of Mandelbrot set = black */
+        buf[iTamanho    ] = 0;
+        buf[iTamanho + 1] = 0;
+        buf[iTamanho + 2] = 0;
+        iTamanho += 3;
     }
-
-    /*write color to the file*/
-    saida.write(color, altura*largura*3);
-	saida.close();
-
-	return 0;
+    else
+    { /* exterior of Mandelbrot set = white */
+        buf[iTamanho    ] = ((IterationMax - Iteration) % 8) *  63;  /* Red */
+        buf[iTamanho + 1] = ((IterationMax - Iteration) % 4) * 127;  /* Green */
+        buf[iTamanho + 2] = ((IterationMax - Iteration) % 2) * 255;  /* Blue */
+        iTamanho += 3;
+    };
 }
