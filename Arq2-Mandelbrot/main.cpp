37ca7f62829b916aa4a7cf35d0f3561f03e4bd18
Power041 <Power041@users.noreply.github.com>
2015-06-17 05:18:21 -0300

    Criação do arquivo .ppm com dimensão 16384x16384

--- Arq2-Mandelbrot/main.cpp
+++ Arq2-Mandelbrot/main.cpp
@@ -1,9 +1,32 @@
+#include <cstring>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main()
+
+int main(int argc, char* argv[])
 {
-    cout << "Hello world!" << endl;
-    return 0;
+	// Arquivo
+	ofstream saida;
+
+    // Criar arquivo com acesso binario
+	saida.open("mandelbrot.ppm", ios::binary | ios::out);
+
+
+    // Tamanho da imagem
+    const int altura    = 16384;
+    const int largura   = 16384;
+    const int tamanho   = altura * largura;
+
+	char * header = new char[20];
+
+	sprintf(header, "P6 %d %d 255", largura, altura);
+
+	// escrever header no arquivo de saida
+	saida.write(header, strlen(header));
+
+	saida.close();
+
+	return 0;
 }
